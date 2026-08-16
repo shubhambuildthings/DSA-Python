4f45a9d69169e352daca101ab5a1f25cd707b2e3
Shubham Sharma <shubhamaiworkspace@gmail.com>
2026-08-16 13:45:06 +0530

    Add Leetcode #2089 - Find Target Indices after Sorting Array

---
+++ Arrays/2089_find_target_indices_after_sorting array.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        answer = []
+
+        for i in range(len(nums)):
+            if nums[i] == target:
+                answer.append(i)
+
+        return answer
