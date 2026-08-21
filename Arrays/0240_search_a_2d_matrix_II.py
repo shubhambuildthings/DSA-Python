31e0b91cee794efd2daf560214301a7c1fcafe16
Shubham Sharma <shubhamaiworkspace@gmail.com>
2026-08-21 22:36:05 +0530

    Add Leetcode #240 - Search a 2D Matrix II

---
+++ Arrays/0240_search_a_2d_matrix_II.py
@@ -0,0 +1,24 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        row = 0
+        col = n - 1
+
+        while row < m and col >= 0:
+            current = matrix[row][col]
+
+            if current == target:
+                return True
+
+            elif current > target:
+                col -= 1
+
+            else:
+                row += 1
+
+        return False
