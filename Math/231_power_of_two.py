fdded06c136eee648aa746260bc3befdeb589269
Shubham Sharma <shubhamaiworkspace@gmail.com>
2026-08-25 23:26:25 +0530

    Add Leetcode #231 - Power of 2

---
+++ Math/231_power_of_two.py
@@ -0,0 +1,9 @@
+class Solution:
+    def ispowerroftwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n > 1:
+            if n % 2 != 0:
+                return False
+            n //= 2
+        return True
