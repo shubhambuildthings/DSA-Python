1931e8d1ec0c71f632d235abf4a93ce057bd737b
Shubham Sharma <shubhamaiworkspace@gmail.com>
2026-08-31 22:45:53 +0530

    Add Leetcode #415 - Add Strings

---
+++ Math/415_add_strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+
+        i, j = len(num1) - 1, len(num2) - 1
+        carry = 0
+        result = []
+
+        while i >= 0 or j >= 0 or carry:
+            x = int(num1[i]) if i >= 0 else 0
+            y = int(num2[j]) if j >= 0 else 0
+            total = x + y + carry
+            carry = total // 10
+            result.append(str(total % 10))
+            i -= 1
+            j -= 1
+
+        return ''.join(reversed(result))
